103895a746cee2189a97b6f5a1e2b1f1af3bcedf
HitakshiAjmera <64796906+HitakshiAjmera@users.noreply.github.com>
2022-02-18 16:58:22 +0530

    Create datatype.py

---
+++ datatype.py
@@ -0,0 +1,40 @@
+# NUMBERIC TYPES
+# int
+A = 20
+print("A is of type ",type(A))
+
+#  float
+A = 8.4
+print("A is of type",type(A))
+
+#  complex
+A = 8+6j
+print("A is of type",type(A))
+
+# SEQUENCE TYPES
+#  list
+A = [1,2,3,4,5]
+print(A)
+print("A is of type",type(A))
+
+#  tuple
+A = (1,2,3,4,5)
+print(A)
+print("A is of type",type(A))
+
+# MAPPING TYPES
+# sets
+A = {1,2,3,4,5}
+print(A)
+print("A is of type",type(A))
+
+# TEXT TYPES
+# string
+A = 'Hitakshi Ajmera'
+print(A)
+print("A is of type",type(A))
+
+# BOOLEAN TYPES
+A = bool(10)
+print(A)
+print("A is of type",type(A))
