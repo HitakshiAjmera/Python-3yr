1c17a68ff7ad6ae4db071ae2fbc485989dd04249
HitakshiAjmera <64796906+HitakshiAjmera@users.noreply.github.com>
2022-03-03 16:17:49 +0530

    Create largest_element_in list.py

---
+++ largest_element_in list.py
@@ -0,0 +1,12 @@
+#WAP to print largest number of a list entered by user
+
+#code:-
+l = list(map(int,input().split()))
+print(l)
+print(max(l)) 
+
+"""output:-
+1 2 10 5 3 
+[1, 2, 10, 5, 3]
+10
+"""
